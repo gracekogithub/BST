37310b6ec6830b43f016d89e4e130f0e30538157
gracekogithub <graceko02011972@gmail.com>
2021-03-02 12:49:34 -0800

    created BST class

---
+++ BSTbigO/BST.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSTbigO
+{
+    class BST
+    {
+        public Node RootNode;
+
+        public BST()
+        {
+
+        }
+
+
+        public void AddNode(Node nodeToAdd)
+        {
+
+
+        }
+
+        public bool SearchNode(int dataToFind)
+        {
+
+        }
+        
+    }
+}
